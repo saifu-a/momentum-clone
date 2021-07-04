a9edfcdcc4d5b4dea561c402f5d35c0a6bdcdd23
Sk Saif Ahmed <sksaifahmed0@gmail.com>
2021-07-05 00:07:10 +0530

    add play button

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -5683,4 +5683,19 @@ setInterval(() => {
   dayElement.innerHTML = day;
 }, 1);
 
+const playButton = document.querySelector(".play-button");
+const videoElement = document.querySelector("video");
+const audioElement = document.querySelector("audio");
+const timeContainer = document.querySelector(".time-container");
+const weatherContainer = document.querySelector(".weather-container");
+const buttonContainer = document.querySelector(".button-container");
+
+playButton.addEventListener("click", () => {
+  buttonContainer.classList.add("hide");
+  timeContainer.classList.remove("hide");
+  weatherContainer.classList.remove("hide");
+  videoElement.play();
+  audioElement.play();
+});
+
 },{"moment":1}]},{},[2]);
--- public/js/index.js
+++ public/js/index.js
@@ -9,3 +9,18 @@ setInterval(() => {
   timeElement.innerHTML = time;
   dayElement.innerHTML = day;
 }, 1);
+
+const playButton = document.querySelector(".play-button");
+const videoElement = document.querySelector("video");
+const audioElement = document.querySelector("audio");
+const timeContainer = document.querySelector(".time-container");
+const weatherContainer = document.querySelector(".weather-container");
+const buttonContainer = document.querySelector(".button-container");
+
+playButton.addEventListener("click", () => {
+  buttonContainer.classList.add("hide");
+  timeContainer.classList.remove("hide");
+  weatherContainer.classList.remove("hide");
+  videoElement.play();
+  audioElement.play();
+});
