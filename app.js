01e76a8698b5b48d83e8a912f663f25ca2b7735e
Sk Saif Ahmed <sksaifahmed0@gmail.com>
2021-07-04 17:38:06 +0530

    add temperature display

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+require("dotenv").config();
 const express = require("express");
+const https = require("https");
 const moment = require("moment");
 
 const app = express();
@@ -9,13 +11,30 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   let icon = "fa-cloud-sun-rain";
-  let temperature = "31°C";
+  let temperature;
   let location = "Kolkata";
 
-  res.render("index", {
-    icon: icon,
-    temperature: temperature,
-    location: location,
+  const query = location;
+  const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+  const unit = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=" +
+    apiKey +
+    "&units=" +
+    unit;
+
+  https.get(url, (response) => {
+    response.on("data", (data) => {
+      const weatherData = JSON.parse(data);
+      temperature = Math.round(weatherData.main.temp);
+      res.render("index", {
+        icon: icon,
+        temperature: temperature,
+        location: location,
+      });
+    });
   });
 });
 
